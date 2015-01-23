66065e298cda4d6f354a905cb2092aac114b493c
FernandoMatias <fernando.matias@sisdado.com.br>
2015-01-23 14:23:35 -0200

    Update teste.js

--- app/view/teste.js
+++ app/view/teste.js
@@ -7,14 +7,36 @@ Ext.define('Touch2Demo.view.teste', {
 		docked: 'top',
 		xtype: 'toolbar',
 		//ui: 'light',
-		title: 'TESTE',
+		title: 'TESTE'
 		
+	    },
+	      {
+		xtype: 'fieldset',
+		title: 'Contato',
+		items: [
+		    {
+			xtype: 'textfield',
+			name: 'nome',
+			label: 'Nome'
+		    },
+		    {
+			xtype: 'textfield',
+			name: 'sobrenome',
+			label: 'Sobrenome'
+		    }
+		]
 	    },
 	    {
 		xtype: 'button',
 		action: 'menuIniciar',
 		ui: 'decline',
 		text: 'Volta'
+	    },
+	    {
+		xtype: 'button',
+		action: 'login',
+		ui: 'decline',
+		text: 'Login'
 	    }
 	]
     }
